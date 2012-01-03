da711146ae98e0d96e83d5d7a6f00aa7d9da01a4
Michael Shields <shields@msrl.com>
2012-01-02 23:29:33 -0800

    android create project -t android-7 -p android -k com.msrl.clocky -a Clocky

---
+++ android/src/com/msrl/clocky/Clocky.java
@@ -0,0 +1,15 @@
+package com.msrl.clocky;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Clocky extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
