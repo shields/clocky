59333fffc8a6afd2e6565efb7497f496b5f8ad57
Michael Shields <shields@msrl.com>
2012-01-03 00:13:42 -0800

    Add a WebView to the Android app.

--- android/src/com/msrl/clocky/Clocky.java
+++ android/src/com/msrl/clocky/Clocky.java
@@ -2,14 +2,20 @@ package com.msrl.clocky;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Clocky extends Activity
 {
+    WebView mWebView;
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
+	
+	mWebView = (WebView) findViewById(R.id.webview);
+	mWebView.getSettings().setJavaScriptEnabled(true);
+	mWebView.loadUrl("http://10.0.1.13:8000");
     }
 }
